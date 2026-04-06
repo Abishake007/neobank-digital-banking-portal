4ec2f9e3a23bbae2e8c65411758bf72bb93e8b58
Abishake007 <abishakearunagirinathan16@gmail.com>
2026-04-06 22:51:19 +0530

    change the base url in frontend

--- frontend-banking-portal-ui/src/app/api.config.ts
+++ frontend-banking-portal-ui/src/app/api.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://neobank-digital-banking-portal.onrender.com";
 
 export const appConfig = {
   providers: [
